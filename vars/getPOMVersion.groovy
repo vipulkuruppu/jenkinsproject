c35ee8cd54925efbf624de55a889fa57da117b1d
Vipul Kuruppu <vipul@Asterix.local>
2018-06-25 01:59:52 +0800

    updating method - Mon 25 Jun 2018 01:59:52 +08

--- vars/getPOMVersion.groovy
+++ vars/getPOMVersion.groovy
@@ -3,5 +3,5 @@ def call(Map pomConfig) {
 
     def version = sh( script: 'date +%d%m%Y.%H%M%S', returnStdout: true).toString().trim()
     echo 'Using version ' +  pomConfig.branchtype + ' ' + pomConfig.pomfile + ' ' + version
-    return "version"
+    return version
 }
