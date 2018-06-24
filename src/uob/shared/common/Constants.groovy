5764dba40dd71f619e41dfe61f86fd85f2cd89db
Vipul Kuruppu <vipul@Asterix.local>
2018-06-25 00:38:50 +0800

    updating method - Mon 25 Jun 2018 00:38:50 +08

--- src/uob/shared/common/Constants.groovy
+++ src/uob/shared/common/Constants.groovy
@@ -3,6 +3,6 @@ package uob.shared.common
 
 class Constants {
 
-    static final BIT_BUCKET_HOME = "${WORKSPACE}/bb_src";
-    static final NEW_SOURCE_HOME = "${WORKSPACE}/new_src";
+    static final BIT_BUCKET_HOME = WORKSPACE + '/bb_src';
+    static final NEW_SOURCE_HOME = WORKSPACE + '/new_src';
 }
