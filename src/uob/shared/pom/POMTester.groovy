8cf646687ca30450b309979280291da0f1722725
Vipul Kuruppu <vipul@Asterix.local>
2018-06-25 01:17:15 +0800

    updating method - Mon 25 Jun 2018 01:17:15 +08

--- src/uob/shared/pom/POMTester.groovy
+++ src/uob/shared/pom/POMTester.groovy
@@ -4,7 +4,7 @@ def readVersion(String branchType, String pomFile){
 
     String version = 'date +%d%m%Y.%H%M%S'.execute().text
     println 'Using version ' +  branchType + ' ' + pomFile + ' ' + version
-    println 'BB Home - ' + ${WORKSPACE} + '/' + Constants.BIT_BUCKET_HOME
+    //println 'BB Home - ' + ${WORKSPACE} + '/' + Constants.BIT_BUCKET_HOME
     sh "echo WORKSPACE - ${WORKSPACE}"
     sh "echo New Sources - ${WORKSPACE}/${Constants.NEW_SOURCE_HOME}"
     return version
