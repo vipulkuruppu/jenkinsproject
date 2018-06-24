2d4e3e9d66aaf6d45997d83ce8ac0a9bffec0339
Vipul Kuruppu <vipul@Asterix.local>
2018-06-24 23:36:11 +0800

    updating method

--- vars/getPOMVersionFromTester.groovy
+++ vars/getPOMVersionFromTester.groovy
@@ -3,7 +3,7 @@ import uob.shared.pom.*
 def call(String branchType, String pomFile) {
 
     def pt = new uob.shared.pom.POMTester()
-    def version = pt.readVersion('Core', 'pom.xml')
-    echo 'Using version ' +  pomConfig.branchtype + ' ' + pomConfig.pomfile + ' ' + version
+    def version = pt.readVersion(branchType, pomFile)
+    echo 'Using version ' +  branchType + ' ' + pomFile + ' ' + version
     return version
 }
