489397dddb59be01898794ad609d5227f3abeffa
Vipul Kuruppu <vipul@Asterix.local>
2018-06-25 00:59:39 +0800

    updating method - Mon 25 Jun 2018 00:59:39 +08

--- src/uob/shared/common/Constants.groovy
+++ src/uob/shared/common/Constants.groovy
@@ -3,6 +3,6 @@ package uob.shared.common
 
 class Constants {
 
-    static final BIT_BUCKET_HOME = '/bb_src';
-    static final NEW_SOURCE_HOME = '/new_src';
+    static final BIT_BUCKET_HOME = 'bb_src';
+    static final NEW_SOURCE_HOME = 'new_src';
 }
---
+++ vars/say.groovy
@@ -0,0 +1,8 @@
+#!/usr/bin/groovy
+def hello(message) {
+    echo "Hello ${message} ..."
+}
+
+def bye(message) {
+    echo "Bye ${message} ..."
+}
