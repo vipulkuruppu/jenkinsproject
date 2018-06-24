0e594f98be88ca559b2d6a23d43d89fdbf0af5ba
Vipul Kuruppu <vipul@Asterix.local>
2018-06-25 01:04:32 +0800

    updating method - Mon 25 Jun 2018 01:04:32 +08

--- vars/say.groovy
+++ vars/say.groovy
@@ -1,8 +1,8 @@
 #!/usr/bin/groovy
 def hello(message) {
-    echo "Hello ${message} ..."
+    echo "Hello " + message + "..."
 }
 
 def bye(message) {
-    echo "Bye ${message} ..."
+    echo "Bye " + message + "..."
 }
