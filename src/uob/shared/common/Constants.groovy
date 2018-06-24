2cc37ddcca7287a473ed079e2ff02f11feaa5855
Vipul Kuruppu <vipul@Asterix.local>
2018-06-25 00:44:50 +0800

    updating method - Mon 25 Jun 2018 00:44:50 +08

--- src/uob/shared/common/Constants.groovy
+++ src/uob/shared/common/Constants.groovy
@@ -2,7 +2,7 @@
 package uob.shared.common
 
 class Constants {
-    def env
-    static final BIT_BUCKET_HOME = env.WORKSPACE + '/bb_src';
-    static final NEW_SOURCE_HOME = env.WORKSPACE + '/new_src';
+
+    static final BIT_BUCKET_HOME = '/bb_src';
+    static final NEW_SOURCE_HOME = '/new_src';
 }
--- src/uob/shared/pom/POMTester.groovy
+++ src/uob/shared/pom/POMTester.groovy
@@ -4,8 +4,8 @@ def readVersion(String branchType, String pomFile){
 
     String version = 'date +%d%m%Y.%H%M%S'.execute().text
     println 'Using version ' +  branchType + ' ' + pomFile + ' ' + version
-    println 'BB Home - ' + Constants.BIT_BUCKET_HOME
+    println 'BB Home - ${WORKSPACE}/' + Constants.BIT_BUCKET_HOME
     sh "echo WORKSPACE - ${WORKSPACE}"
-    sh "echo New Sources - ${Constants.NEW_SOURCE_HOME}"
+    sh "echo New Sources - ${WORKSPACE}/${Constants.NEW_SOURCE_HOME}"
     return version
 }
