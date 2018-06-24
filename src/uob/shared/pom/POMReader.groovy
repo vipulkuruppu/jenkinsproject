5ba98afd68bc65e3a66e85f44957e6d6ee40f30c
Vipul Kuruppu <vipul@Asterix.local>
2018-06-24 17:26:56 +0800

    updating method

--- src/uob/shared/pom/POMReader.groovy
+++ src/uob/shared/pom/POMReader.groovy
@@ -1,12 +1,12 @@
 package uob.shared.pom
 
-//class POMReader implements Serializable {
+class POMReader implements Serializable {
 
-    def readVersion(String branchType, String pomfile){
+    static def readVersion(String branchType, String pomfile){
 
         String version = sh( script: 'date +%d%m%Y.%H%M%S', returnStdout: true).toString().trim()
         echo 'Using version ' +  pomConfig.branchtype + ' ' + pomConfig.pomfile + ' ' + version
         return version
     }
     return this
-//}
+}
