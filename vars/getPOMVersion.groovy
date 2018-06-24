5d70121724aae3f3df5e88210261da7a04104209
Vipul Kuruppu <vipul@Asterix.local>
2018-06-24 13:38:48 +0800

    update utils

---
+++ vars/getPOMVersion.groovy
@@ -0,0 +1,13 @@
+#!/usr/bin/groovy
+def call(body) {
+    // evaluate the body block, and collect configuration into the object
+    def pomConfig = [:]
+    body.resolveStrategy = Closure.DELEGATE_FIRST
+    body.delegate = pomConfig
+    body()
+
+    def version = sh( script: 'date +%d%m%Y.%H%M%S', returnStdout: true).toString().trim()
+    echo 'Using new version ' +  pomConfig.pomfile + ' ' + pomConfig.pompath + ' ' + version
+    return version
+
+}
