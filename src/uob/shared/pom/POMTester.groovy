4c41c41675a58787198b526be061f68b7090fc2f
Vipul Kuruppu <vipul@Asterix.local>
2018-06-25 00:24:13 +0800

    updating method

--- src/uob/shared/pom/POMTester.groovy
+++ src/uob/shared/pom/POMTester.groovy
@@ -4,6 +4,7 @@ def readVersion(String branchType, String pomFile){
 
     String version = 'date +%d%m%Y.%H%M%S'.execute().text
     println 'Using version ' +  branchType + ' ' + pomFile + ' ' + version
+    sh "pwd"
     return version
 }
 
