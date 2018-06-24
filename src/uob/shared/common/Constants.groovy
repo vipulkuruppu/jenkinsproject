509c966bbb192bd14dd5f853a989e0546be3ca17
Vipul Kuruppu <vipul@Asterix.local>
2018-06-25 00:35:40 +0800

    updating method - Mon 25 Jun 2018 00:35:40 +08

--- src/uob/shared/common/Constants.groovy
+++ src/uob/shared/common/Constants.groovy
@@ -3,13 +3,6 @@ package uob.shared.common
 
 class Constants {
 
-    static final JK_WORKSPACE = 'enter sonarqube your url here';
-    static final GITLAB_WEB_BASE_URL = 'enter gitlab url here';
-    static final GITLAB_API_BASE_URL = 'enter gitlab api url here';
-    static final GITLAB_CHECKOUT_BASE_URL = 'enter gitlab checkout url here';
-    static final GITLAB_VERSION = '8.6';
-    static final GITLAB_CREDENTIALS_ID = 'enter credentials here';
-    static final GITLAB_API_TOKEN_CREDENTIALS_ID = 'enter api token here';
-    static final ENVIRONMENT_DOMAIN_NAME = '.tld'
-    static final ENVIRONMENT_TENANT = 'foobar';
+    static final BIT_BUCKET_HOME = "${WORKSPACE}/bb_src";
+    static final NEW_SOURCE_HOME = "${WORKSPACE}/new_src";
 }
--- src/uob/shared/pom/POMTester.groovy
+++ src/uob/shared/pom/POMTester.groovy
@@ -1,9 +1,11 @@
 package uob.shared.pom
-
+import uob.shared.common.Constants
 def readVersion(String branchType, String pomFile){
 
     String version = 'date +%d%m%Y.%H%M%S'.execute().text
     println 'Using version ' +  branchType + ' ' + pomFile + ' ' + version
-    sh "echo ${WORKSPACE}"
+    println 'BB Home - ' + Constants.BIT_BUCKET_HOME
+    sh "echo WORKSPACE - ${WORKSPACE}"
+    sh "echo New Sources - ${Constants.NEW_SOURCE_HOME}"
     return version
 }
