fbd486c64ca3e0903da56f42db1b0727580e29e3
Vipul Kuruppu <vipul@Asterix.local>
2018-06-27 02:59:35 +0800

    updating method - Wed 27 Jun 2018 02:59:35 +08

--- resources/shell/hello.sh
+++ resources/shell/hello.sh
@@ -1,2 +1,2 @@
 #!/bin/bash
-echo "Hello $0 ..."
+echo "Hello $0 $1 ..."
--- vars/say.groovy
+++ vars/say.groovy
@@ -7,7 +7,7 @@ def bye(message) {
     echo "Bye " + message + "..."
 }
 
-def hellofromsh(message){
+def hellofromsh(message1, message2){
     def shell_script = libraryResource 'shell/hello.sh'
-    sh "bash -c '${shell_script}' ${message}"
+    sh "bash -c '${shell_script}' ${message1} ${message2}"
 }
